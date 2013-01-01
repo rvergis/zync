6466a9c4463d4efd2d2504e1bf5f454eca895357
rvergis <ron.vergis@gmail.com>
2012-12-31 19:02:19 -0800

    added test

--- zync/ztool.c
+++ zync/ztool.c
@@ -14,7 +14,7 @@ int zmake(char * sourcefile_url, char * targetfile_url, zync_block_size_t block_
     FILE *in_file = fopen(sourcefile_url, "r");
     FILE *out_file = fopen(targetfile_url, "w");
     
-    struct zync_state *zs = init_zync_state(0, 0);
+    struct zync_state *zs = init_zync_state(0, 0, 0, NULL);
     int generate_success =  generate_zync_state(in_file, zs, block_size);
     if (generate_success < 0)
     {
